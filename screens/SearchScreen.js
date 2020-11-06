eaaa01a54bd631dc935ba08f77e4d9f0654eeb92
Aarushamit <64181805+Aarushamit@users.noreply.github.com>
2020-11-06 19:16:04 +0530

    Add files via upload

---
+++ screens/SearchScreen.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Text, ScrollView, View, FlatList } from 'react-native';
+import db from '../config'
+
+export default class Searchscreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+allTransactions: [],
+lastVisibleTransaction: null,
+    }
+}
+
+componentDidMount =async () => {
+  const query =await  db.collection("transactions").get()
+  query.docs.map((doc)=>{
+      this.setState({
+          allTransactions: [...this.state.allTransactions, doc.data()]
+
+      })
+  console.log(this.state.allTransactions)
+  })
+}
+
+fetchMoreTransactions = async () => {
+  const query = await db.collection("transactions").startAfter(this.state.lastVisibleTransaction).limit(10).get()
+  query.docs.map((doc)=>{
+    this.setState({
+        allTransactions: [...this.state.allTransactions, doc.data()],
+        lastVisibleTransaction: doc
+
+
+    })
+})
+}
+
+
+    render() {
+      return (
+      
+              <FlatList
+              data = {this.state.allTransactions}
+              renderItem = {({item})=> {
+                  <View style = {{borderBottomWidth: 2}}>
+                        <Text> {"Book Id: " + item.bookId}   </Text>
+                         <Text> {"Student Id: " + item.studentId}   </Text>
+                         <Text> {"Transaction Type:" + item.transactionType}   </Text>
+                         <Text> {"date: " + item.date}   </Text>
+
+                         
+                  </View>
+
+
+              }}
+
+              keyExtractor = {(item, index)=> index.toString()}
+              onEndReached = {this.fetchMoreTransactions}
+              onEndReachedThreshold = {0.7}
+
+              />
+            
+      );
+    }
+  }
